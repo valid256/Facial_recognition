a6cf802144133295b157d30f7678483c6685a965
Jacob Wilskey <132802690+valid256@users.noreply.github.com>
2024-01-28 16:59:43 +0800

    Create 自动时钟.py
    史

---
+++ 自动时钟.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from time import strftime
+
+# 创建一个窗口
+window = tk.Tk()
+window.title("自动时钟")
+
+# 定义一个函数来获取当前时间并更新标签上的时间
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+# 创建一个标签用于显示时间
+label = tk.Label(window, font=('calibri', 40, 'bold'), background='purple', foreground='white')
+label.pack(anchor='center')
+
+# 调用time函数开始更新时间
+time()
+
+# 运行窗口主循环
+window.mainloop()
